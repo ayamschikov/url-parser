b2958c1fba83836af0136c8223de744abb07bec2
Aleksandr Yamschikov <yamshchikov.as73@gmail.com>
2020-02-12 00:46:43 +0400

    Add env file

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'roda'
+require_relative 'db'
 
 class App < Roda
 
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require_relative 'spec_helper'
 
 describe Site, type: :model do
   it 'should create new row' do
