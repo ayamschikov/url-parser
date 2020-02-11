01aae7081727388550c6c369e583c28f47d69952
Aleksandr Yamschikov <yamshchikov.as73@gmail.com>
2020-02-11 23:43:00 +0400

    Add site database migration

---
+++ migrate/001_create_sites.rb
@@ -0,0 +1,14 @@
+Sequel.migration do
+  up do
+    create_table(:sites) do
+      primary_key :id
+      String :url, null: false
+      String :title, null: false
+      Integer :status, null: false
+    end
+  end
+
+  down do
+    drop_table(:sites)
+  end
+end
