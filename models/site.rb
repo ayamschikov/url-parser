8ddbc833f3229be0d6b2e669cd984efb8607fd46
Aleksandr Yamschikov <yamshchikov.as73@gmail.com>
2020-02-12 15:55:10 +0400

    Add model validation

--- models/site.rb
+++ models/site.rb
@@ -1,2 +1,8 @@
 class Site < Sequel::Model
+  plugin :validation_helpers
+
+  def validate
+    validates_integer :status
+    validates_presence %i[title url status]
+  end
 end
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -3,8 +3,14 @@ require 'spec_helper'
 
 describe Site, type: :model do
   it 'should create new row' do
-    sites = DB[:sites]
-    sites.insert(url: 'url1', title: 'title1', status: 200)
-    expect(sites.count).to eq(1)
+    Site.create(url: 'url1', title: 'title1', status: 200)
+    expect(Site.count).to eq(1)
+  end
+
+  it "should raise error if there's not enough fields" do
+    site = Site.new(url: 'url')
+
+    expect(site.valid?).to be_falsey
+    expect{ site.save }.to raise_error(Sequel::ValidationFailed)
   end
 end
